99abec07e15736a7d16567afb761935965d9f04a
Renato de Leão <renatodeleao@gmail.com>
2016-12-01 16:30:51 +0800

    reference the Component on own methods

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,28 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	// constructor() {
+	// 	super();
+	// 	this.goToStore = this.goToStore.bind(this);
+	// }
+
+	goToStore(event){
+		event.preventDefault();
+		// grab text from box
+		const value = this.storeInput.value;
+		console.log(value);
+		// second go to store/storeID
+	}
+
 	render(){
 		return (
-			<form className="store-selector">
+			/* old school {this.goToStore.bind(this)}*/
+			<form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
 				{/* HTML Comment */}
 				<h2>Please Enter a Store</h2>
-				<input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+				<input type="text" required placeholder="Store Name" 
+					defaultValue={getFunName()} 
+					ref={(input) => {this.storeInput = input} } />
 				<button type="submit">Visit Store -></button>
 			</form>
 		) 
