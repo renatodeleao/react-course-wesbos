b38f3468bcf09fd9009a054a200eef26789b5c56
Renato de Leão <renatodeleao@gmail.com>
2016-12-01 19:50:48 +0800

    About state
    Managin state on upper wrapper comp app
    state in es6 is now an object in the constructor (getInitialState)
    copy state state before modify with ...
    Create methods for modifiy state at state component and pass it via props to child components

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,28 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+	constructor() {
+		super();
+
+		this.addFish = this.addFish.bind(this);
+		//get initial state
+		this.state = {
+			fishes: {},
+			order: {}
+		};
+	}
+
+	addFish(fish){
+		//update state, first take a copy of state (perf)
+		const fishes = {...this.state.fishes};
+		//add in new fish with unique id
+		const timestamp = Date.now();
+		fishes[`fish-${timestamp}`] = fish;
+
+		//set state
+		this.setState({fishes: fishes })
+	}
+
 	render(){
 		return (
 			<div className="catch-of-the-day">
@@ -11,7 +33,7 @@ class App extends React.Component {
 					<Header tagline="Fresh Seafood Market" />
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={this.addFish} />
 			</div>
 		) 
 	}
