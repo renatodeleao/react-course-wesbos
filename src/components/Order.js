618ec2779f49bee9acff850dfec3dd21c5a76fb0
Renato de Leão <renatodeleao@gmail.com>
2016-12-02 15:15:30 +0800

    Displaying order state with renderFunction not with component
    Although it could be possible to use a small component, this will not be reused anywhere but order so it can be a render method inside the order itself:

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,9 +1,60 @@
 import React from "react";
+import { formatPrice } from "../helpers";
 
 class Order extends React.Component {
-	render() {
+	constructor() {
+		super();
+
+		this.renderOrder = this.renderOrder.bind(this);
+	}
+
+	renderOrder(key) {
+		const { order, fishes } = this.props;
+		const fish = fishes[key];
+		const count = order[key];
+
+		if(!fish || fish.status === 'unavailable'){
+			return (
+				<li key={key}>
+					Sorry, {fish ? fish.name : 'fish' } is no longer available!
+				</li>
+			)
+		}
 		return (
-			<p>Order</p>
+			<li key={key}>
+				<span>{count}lbs {fish.name}</span>
+				<span className="price">{formatPrice(count * fish.price)}</span>
+			</li>
+		)
+	}
+
+	render() {
+		const { order, fishes } = this.props;
+		const orderIds = Object.keys(order);
+		const total = orderIds.reduce((prevTotal, key) => {
+			const fish = fishes[key];
+			const count = order[key];
+			const isAvailable = fish && fish.status === 'available';
+
+			if (isAvailable) {
+				return prevTotal + (count * fish.price || 0);
+			}
+
+			return prevTotal;
+		}, 0);
+
+		return (
+			<div className="order-wrap">
+				<h2>Your order</h2>
+
+				<ul className="order">
+					{orderIds.map(this.renderOrder)}
+					<li className="total">
+						<strong>Total:</strong>
+						{formatPrice(total)}
+					</li>
+				</ul>
+			</div>
 		)
 	}
 }
