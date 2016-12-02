485935fdb7cb10824314f4bc3635fca585f9d25f
Renato de Leão <renatodeleao@gmail.com>
2016-12-02 16:02:54 +0800

    Persistent temporary data: localStorage to the rescue
    using component lifeCycle events to check if data was modified. Save order part of the state to localStorage so when page refresh it persists, like cookies.-

--- src/components/App.js
+++ src/components/App.js
@@ -21,10 +21,26 @@ class App extends React.Component {
 	}
 
 	componentWillMount() {
+		//right before <App /> rendered
 		this.ref= base.syncState(`${this.props.params.storeId}/fishes`, {
 			context: this,
 			state: 'fishes'
 		});
+
+		//check if order in LS
+		const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+		if(localStorageRef){
+			//updated app Component order state
+			this.setState({
+				order: JSON.parse(localStorageRef)
+			});
+		}
+	}
+
+	componentWillUpdate(nextProps, nextState) {
+		// console.log("something Changed");
+		// console.log({nextProps, nextState})
+		localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order))
 	}
 
 	//remove events when change store
@@ -77,7 +93,9 @@ class App extends React.Component {
 						}
 					</ul>
 				</div>
-				<Order fishes={this.state.fishes} order={this.state.order} />
+				<Order fishes={this.state.fishes}
+					    order={this.state.order} 
+					    params={this.props.params} />
 				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 		) 
